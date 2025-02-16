f9bed7a8c9e71d15da41141675db7dc240a04f76
hhuzzz <hhuzzz124@163.com>
2025-02-16 13:28:44 +0800

    initialize project

---
+++ src/main/java/me/huan/aqs/Test.java
@@ -0,0 +1,13 @@
+package me.huan.aqs;
+
+/**
+ * Test
+ *
+ * @author hhuzzz
+ * @version 2025/02/16 13:28
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
